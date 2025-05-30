0ea482b3280803e328cfefbaf5dda19e29dc375f
Shyhoon Moontaka <130297057+Shyhoon-Moontaka@users.noreply.github.com>
2025-05-30 19:56:13 +0600

    Update index.js

--- ai-server/index.js
+++ ai-server/index.js
@@ -6,7 +6,7 @@ import reportRoutes from "./routes/report.js";
 const app = express();
 
 const corsConfig = {
-  origin: "*",
+  origin: process.env.BASE_URL || "*",
   credentials: true,
 };
 const PORT = process.env.PORT || 5000;
